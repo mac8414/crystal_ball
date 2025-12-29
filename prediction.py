f9eed3c9807a117248ecd915f286cfa3842f59ec
mac8414 <huntermk10@gmail.com>
2025-12-29 16:47:25 -0700

    Updated to print genre's as well

--- prediction.py
+++ prediction.py
@@ -4,7 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 from collections import Counter
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
 
 load_dotenv()
 
@@ -18,3 +18,15 @@ results = sp.current_user_recently_played(limit=50)
 for i in results['items']:
     track = i['track']
     print(f"Played {track['name']} by {track['artists'][0]['name']}")
+
+genres = []
+for i in results['items']:
+    # get id
+    # get artist
+    # get genre and put it into genres
+    artist_id = i['track']['artists'][0]['id']
+    artist = sp.artist(artist_id)
+    artist_genres = artist['genres']
+    genres.extend(artist_genres)
+    print(f"Artist genres: {artist_genres}")
+
